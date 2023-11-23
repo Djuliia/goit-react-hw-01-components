1fa9eb6dc470a42faf12340b693acee406f381a2
Yuliia Chaika <djuliya72@ukr.net>
2023-11-23 19:42:54 +0200

    create data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,10 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from '../../user.json';
-import data from '../../data.json';
-import friends from '../../friends.json';
-import transactions from '../../transactions.json';
+import user from '../../data/user.json';
+import data from '../../data/data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 import { Container } from './App.styled';
 import { GlobalStyle } from 'components/GlobalStyle';
 
